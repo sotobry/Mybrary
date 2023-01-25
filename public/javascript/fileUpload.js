8a6c3fb22a50f7537db70f8ad4f4f14971429406
Bryann Sotomayor <bryann.sotomayor@gmail.com>
2023-01-24 18:39:17 -0800

    Finish project.

--- public/javascript/fileUpload.js
+++ public/javascript/fileUpload.js
@@ -1,15 +1,37 @@
-// Register the plugins
-FilePond.registerPlugin(
-  FilePondPluginFileEncode,
-  FilePondPluginImagePreview,
-  FilePondPluginImageResize
-);
+const rootStyles = window.getComputedStyle(document.documentElement);
 
-// ... FilePond initialization code here
-FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150,
-});
+const ready = () => {
+  const coverWidth = parseFloat(
+    rootStyles.getPropertyValue('--book-cover-width-lg')
+  );
+  const coverAspectRatio = parseFloat(
+    rootStyles.getPropertyValue('--book-cover-aspect-ratio')
+  );
 
-FilePond.parse(document.body);
+  const coverHeight = coverWidth / coverAspectRatio;
+
+  // Register the plugins
+  FilePond.registerPlugin(
+    FilePondPluginFileEncode,
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize
+  );
+
+  // ... FilePond initialization code here
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight,
+  });
+
+  FilePond.parse(document.body);
+};
+
+if (
+  rootStyles.getPropertyValue('--book-cover-width-lg') != null &&
+  rootStyles.getPropertyValue('--book-cover-width-lg') !== ''
+)
+  ready();
+else {
+  document.getElementById('main-css').addEventListener('load', ready);
+}
